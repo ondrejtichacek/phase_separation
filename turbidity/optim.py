28fdbbb7af0df9523b15b5587bef59da1ea5a60e
Ondřej Ticháček <ondrejtichacek@gmail.com>
2021-11-12 15:02:49 +0100

    turbidity fit

--- turbidity/optim.py
+++ turbidity/optim.py
@@ -1,5 +1,5 @@
 import os
-from scipy.optimize import differential_evolution
+from scipy.optimize import differential_evolution, dual_annealing
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -13,10 +13,13 @@ from bp import bp_main
 class Optimizer():
 
     def __init__(self,
+        lattice_connectivity,
         sel = 'atp',
         vol_frac_scaling_x = 10,
         vol_frac_scaling_y = 10,
         ):
+
+        self.lattice_connectivity = lattice_connectivity
 
         self.sel = sel
         
@@ -25,8 +28,51 @@ class Optimizer():
         self.vol_frac_scaling_x = vol_frac_scaling_x
         self.vol_frac_scaling_y = vol_frac_scaling_y
 
+    def load_mock_data(self, fname):
 
-    def load_exp_data(self):
+        data = np.loadtxt(fname)
+
+        x = data[:,0]
+        y = data[:,1]
+        sep = data[:,2]
+
+        ind = ((x + y) < 1) & (x > 0) & (y > 0)
+
+        x = x[ind]
+        y = y[ind]
+
+        sep = sep[ind]
+
+        self.x = np.ascontiguousarray(x, dtype=np.double)
+        self.y = np.ascontiguousarray(y, dtype=np.double)
+        self.sep = np.ascontiguousarray(sep, dtype=np.int32)
+
+        n = self.x.size
+
+        min_x = np.min(self.x)
+        min_y = np.min(self.y)
+        max_x = np.max(self.x)
+        max_y = np.max(self.y)
+
+        tolx = 1e-5
+        toly = 1e-5
+
+        is_on_boundary = np.zeros(n, dtype=np.int32)
+
+        for i in range(n):
+            if ((self.x[i] < min_x + tolx)
+                or (self.x[i] > max_x - tolx)
+                or (self.y[i] < min_y + toly)
+                or (self.y[i] > max_y - toly)):
+                is_on_boundary[i] = 1
+            else:
+                is_on_boundary[i] = 0
+
+        is_on_boundary = np.zeros_like(is_on_boundary)
+
+        self.is_on_boundary = is_on_boundary
+
+    def load_exp_data(self, limit=None, extended=True):
 
         f_sep = {}
         f_mix = {}
@@ -40,21 +86,44 @@ class Optimizer():
         sep_exp = np.loadtxt(f_sep)
         mix_exp = np.loadtxt(f_mix)
 
-        sep_exp[:,0] *= self.vol_frac_scaling_x
-        mix_exp[:,0] *= self.vol_frac_scaling_x
-
-        sep_exp[:,1] *= self.vol_frac_scaling_y
-        mix_exp[:,1] *= self.vol_frac_scaling_y
-
         x = np.concatenate([sep_exp[:,0], mix_exp[:,0]])
         y = np.concatenate([sep_exp[:,1], mix_exp[:,1]])
         sep = np.concatenate([np.ones_like(sep_exp[:,0]), np.zeros_like(mix_exp[:,0])])
 
-        ind = ((x + y) < 1) & (x > 0)  & (y > 0)
+        if limit is not None:
+            ind = ((limit['x'][0] < x) 
+                    & (x < limit['x'][1]) 
+                    & (limit['y'][0] < y) 
+                    & (y < limit['y'][1]))
+
+            x = x[ind]
+            y = y[ind]
+
+            sep = sep[ind]
+
+        x *= self.vol_frac_scaling_x
+        y *= self.vol_frac_scaling_y
+
+        ind = ((x + y) < 1) & (x > 0) & (y > 0)
 
         x = x[ind]
         y = y[ind]
+
         sep = sep[ind]
+
+        if extended is True:
+            y1 = np.linspace(np.min(y), 0.9, 40)
+            x1 = np.full_like(y1, np.min(x))
+
+            x2 = np.linspace(np.min(x), 0.9, 40)
+            y2 = np.full_like(x2, np.min(y))
+
+            x = np.concatenate([x, x1, x2])
+            y = np.concatenate([y, y1, y2])
+
+            sep_extra = np.zeros_like(x, dtype=np.int32)
+
+            sep = np.concatenate([sep, sep_extra])
 
         self.x = np.ascontiguousarray(x, dtype=np.double)
         self.y = np.ascontiguousarray(y, dtype=np.double)
@@ -105,6 +174,12 @@ class Optimizer():
         y = self.y
         sep_exp = self.sep
 
+        num_mix = np.count_nonzero(sep_exp == 0)
+        num_sep = np.count_nonzero(sep_exp == 1)
+
+        num_mix /= sep_exp.size
+        num_sep /= sep_exp.size
+
         n = x.size
 
         t_double = np.float64
@@ -129,17 +204,13 @@ class Optimizer():
 
         args = [
             x, y,
-            hx_xy,
-            hy_xy,
-            hx_xpy,
-            hy_xpy,
-            hx_xyp,
-            hy_xyp,
+            hx_xy, hy_xy,
+            hx_xpy, hy_xpy,
+            hx_xyp, hy_xyp,
             mp_, mm_, xp_, xm_,
-            sep_model,
-            sep_exp,
+            sep_model, sep_exp,
             is_on_boundary,
-            n
+            n, num_mix, num_sep,
         ]
 
         bounds = [params[key] for key in params]
@@ -147,21 +218,30 @@ class Optimizer():
         # nc = os.cpu_count() / 2
 
         popsize = 2*64
+        popsize = 32
+        popsize = 16
 
         maxeval = (maxiter + 1) * popsize * len(bounds)
 
         print(f"Maximum fun evals: (maxiter + 1) * popsize * len(x) = {maxeval}")
         print(f"  = {maxeval * 0.064 / 60} min")
 
-        result = differential_evolution(self.fun_cy, bounds, args,
-            popsize=popsize,
-            polish=False,
-            workers=1,
-            # updating='deferred',
-            maxiter=maxiter)
+        self.min_cost = np.inf
+        self.it = 0
 
-        print("best_x: " + " ".join([f"{x}" for x in result.x]))
-        print("best_x: " + ", ".join([f"{x}" for x in result.x]))
+        result = dual_annealing(self.fun_cy, bounds, args)
+
+        # result = differential_evolution(self.fun_cy, bounds, args,
+        #     popsize=popsize,
+        #     # polish=False,
+        #     # recombination=0.2,
+        #     # mutation=(0.5, 1.5)
+        #     workers=1,
+        #     # updating='deferred',
+        #     maxiter=maxiter)
+
+        print("best_x: " + " ".join([f"{x:3g}" for x in result.x]))
+        print("best_x: " + ", ".join([f"{x:3g}" for x in result.x]))
         print(result)
         print(result.fun)
 
@@ -174,21 +254,24 @@ class Optimizer():
             hx_xyp, hy_xyp,
             mp_, mm_, xp_, xm_,
             sep, sep_exp,
-            is_on_boundary, n):
+            is_on_boundary, n, nmix, nsep):
+
         
-        # lp = P[0]
-        # lm = P[1]
-        # l0 = P[2]
-        # Jp = P[3]
-        # Jm = P[4]
-        # Jpm = P[5]
-        # J0 = P[6]
-        # J0p = P[7]
-        # J0m = P[8]
+        lp = P[0]
+        lm = P[1]
+        l0 = P[2]
+        Jp = P[3]
+        Jm = P[4]
+        Jpm = P[5]
+        J0 = P[6]
+        J0p = P[7]
+        J0m = P[8]
+        scale = P[9]
+        rel_scale_y = P[10]
 
         cost = bp_main(
-            # lp, lm, l0, Jp, Jm, Jpm, J0, J0p, J0m, 
-            *P,
+            lp, lm, l0, Jp, Jm, Jpm, J0, J0p, J0m,
+            scale, scale * rel_scale_y,
             x, y,
             hx_xy, hy_xy,
             hx_xpy, hy_xpy,
@@ -196,7 +279,17 @@ class Optimizer():
             mp_, mm_, xp_, xm_,
             sep, sep_exp,
             is_on_boundary,
-            n)
+            self.lattice_connectivity,
+            n, nmix, nsep)
+
+        self.it += 1
+
+        if np.mod(self.it, 100) == 0:
+            print(f"iteration {self.it}")
+
+        if cost < self.min_cost:
+            print(f"{cost:2g} : " + ", ".join([f"{x:3g}" for x in P]))
+            self.min_cost = cost
 
         return cost
 
--- turbidity/optimize_cy.py
+++ turbidity/optimize_cy.py
@@ -3,14 +3,18 @@ import numpy as np
 import matplotlib as mpl
 from bp import bp_main
 from common import timer
+from numpy.lib.function_base import append
 from optim import Optimizer
 
 import argparse
 parser = argparse.ArgumentParser()
 
-parser.add_argument("molecule", help="")
+parser.add_argument("--molecule", default="all", help="")
 parser.add_argument("--maxiter", type=int, default=100, help="")
 parser.add_argument("--plot", action="store_true", default=False, help="")
+parser.add_argument("--test", action="store_true", default=False, help="")
+parser.add_argument("--mock", action="store_true", default=False, help="")
+parser.add_argument("--create-mock-data", action="store_true", default=False, help="")
 
 args = parser.parse_args()
 
@@ -20,7 +24,44 @@ scale = {
     'amp': 232,
     'adp': 50,
     'atp': 20,
-    # 'poly_arg': 20,
+    'poly_arg': 20,
+}
+
+scale = {
+    'mock': 1,
+    'amp': 1,
+    'adp': 1,
+    'atp': 1,
+}
+
+exp_limit = {
+    'atp': {
+        'x': [0, 1],
+        'y': [0, 1],
+    },
+    'adp': {
+        'x': [0, 0.001],
+        'y': [0.0005, 1],
+    },
+    'amp': {
+        'x': [0, 0.0012],
+        'y': [0, 1],
+    },
+}
+
+exp_limit_plot = {
+    'atp': {
+        'x': [0, 1],
+        'y': [0, 1],
+    },
+    'adp': {
+        'x': [0, 1],
+        'y': [0, 1],
+    },
+    'amp': {
+        'x': [0, 1],
+        'y': [0, 1],
+    },
 }
 
 def main():    
@@ -28,19 +69,20 @@ def main():
     sel = args.molecule
 
     opt = Optimizer(
+        lattice_connectivity=6,
         sel=sel,
         vol_frac_scaling_x=scale[sel],
         # vol_frac_scaling_y=scale['poly_arg'],
         vol_frac_scaling_y=scale[sel],
     )
 
-    opt.load_exp_data()
+    opt.load_exp_data(exp_limit_plot[sel])
 
     # p ... positive ... ARG
     # m ... negative ... ATP/ADP/AMP
     # 0 ... solvent ... water
 
-    J = 20
+    J = 10
 
     bounds = {
         'lp': (0, 10), # rel. to l0
@@ -49,35 +91,97 @@ def main():
         'Jp': (-J, 0),
         'Jm': (-J, 0),
         'Jpm': (0, J),
-        'J0': (0, J),
-        'J0p': (0, J),
-        'J0m': (0, J),
+        'J0': (-1, 1),
+        'J0p': (-1, 1),
+        'J0m': (-1, 1),
+        'scale': (1, 100),
+        'rel_scale_y': (0.1, 10),
     }
 
     opt.optimize_cy(bounds, maxiter=args.maxiter)
 
+def main_mock():    
+
+    sel = args.molecule
+
+    opt = Optimizer(
+        lattice_connectivity=4,
+        sel='mock',
+        vol_frac_scaling_x=1,
+        vol_frac_scaling_y=1,
+    )
+
+    # opt.load_mock_data('../nino/phase_diag_0.txt')
+    opt.load_mock_data('mock_data.txt')
+
+    J1 = 4
+    eps = 0.4
+    J0 = 0
+
+    bounds = {
+        'lp': (1, 1+1e-10), # rel. to l0
+        'lm': (1, 1+1e-10), # rel. to l0
+        'l0': (1, 1+1e-10),  # reference
+        'Jp': (-1-eps, -1+eps),
+        'Jm': (-1-eps, -1+eps),
+        'Jpm': (3-eps, 3+eps),
+        'J0': (-J0, J0+1e-10),
+        'J0p': (-J0, J0+1e-10),
+        'J0m': (-J0, J0+1e-10),
+        'scale': (1, 1+1e-10),
+        'rel_scale_y': (1, 1+1e-10),
+    }
+
+    opt.optimize_cy(bounds, maxiter=args.maxiter)
+
+@timer
+def bp_wrapper(*args):
+    err = bp_main(*args)
+    if err < 0:
+        raise(ValueError("Not converged"))
+    return err
 
 def test(
     sel, 
+    # lp = 2,
+    # lm = 1,
+    # l0 = 1,
+    # Jp = -0.2,
+    # Jm = -20,
+    # Jpm = 1.1*2,
+    # J0 =  -0.2,
+    # J0p = -0.2,
+    # J0m = -0.1,
     lp = 1,
     lm = 1,
     l0 = 1,
-    Jp = -1,
-    Jm = -1,
-    Jpm = 3,
-    J0 = 0.1,
-    J0p = 0.5,
-    J0m = 0.2,
+    Jp = -1.3,
+    Jm = -2,
+    Jpm = 2.2,
+    J0 =  0.1,
+    J0p = 0.2,
+    J0m = 0.6,
+    opt_scale = 1,
+    opt_rel_scale_y = 1,
 ):
 
+    if sel == 'mock':
+        lattice_connectivity = 4
+    else:
+        lattice_connectivity = 6
+
     opt = Optimizer(
+        lattice_connectivity=lattice_connectivity,
         sel=sel,
-        vol_frac_scaling_x=scale[sel],
+        vol_frac_scaling_x=scale[sel] * opt_scale,
         # vol_frac_scaling_y=scale['poly_arg'],
-        vol_frac_scaling_y=scale[sel],
+        vol_frac_scaling_y=scale[sel] * opt_scale * opt_rel_scale_y,
     )
 
-    opt.load_exp_data()
+    if sel == 'mock':
+        opt.load_mock_data('../nino/phase_diag_0.txt')
+    else:
+        opt.load_exp_data(exp_limit_plot[sel], extended=False)
 
     x = opt.x
     y = opt.y
@@ -102,42 +206,19 @@ def test(
     sep = np.ones(n, dtype=np.int32)
     is_on_boundary = np.ascontiguousarray(opt.is_on_boundary)
 
-    @timer
-    def bp_wrapper(*args):
-        return bp_main(*args)
-
     err = bp_wrapper(
         lp, lm, l0, Jp, Jm, Jpm, J0, J0p, J0m, 
+        1, 1, 
         x, y,
         hx_xy, hy_xy,
         hx_xpy, hy_xpy,
         hx_xyp, hy_xyp,
         mp_, mm_, xp_, xm_,
         sep, sep_exp,
-        is_on_boundary, n
+        is_on_boundary, opt.lattice_connectivity, n, 1, 1,
     )
 
     print(err)
-
-    # i1 = sep != 0
-    # i2 = sep == 0
-
-    # plt.figure()
-    # plt.plot(x[i1], y[i1], 's', color='r', alpha=0.3)
-    # plt.plot(x[i2], y[i2], 's', color='#999', alpha=0.3)
-    # plt.xlabel(sel)
-    # plt.ylabel('polyARG')
-    # #plt.show()
-
-    # i1 = sep_exp != 0
-    # i2 = sep_exp == 0
-
-    # #plt.figure()
-    # plt.plot(x[i1], y[i1], 's', color='b', alpha=0.3)
-    # plt.plot(x[i2], y[i2], 's', color='#999', alpha=0.3)
-    # plt.xlabel(sel)
-    # plt.ylabel('polyARG')
-    # plt.show()
 
     i1 = sep != 0
     i2 = sep == 0
@@ -152,10 +233,10 @@ def test(
     plt.plot(x[i2 & i3], y[i2 & i3], 's', color='#222', alpha=0.3)
     plt.xlabel(sel)
     plt.ylabel('polyARG')
-    plt.show()
+    # plt.show()
 
-    xp = np.linspace(0.001, 1, 200)
-    yp = np.linspace(0.001, 1, 200)
+    xp = np.linspace(0.001, 0.75, 128)
+    yp = np.linspace(0.001, 0.75, 128)
     X, Y = np.meshgrid(xp, yp)
     X = X.flatten()
     Y = Y.flatten()
@@ -184,22 +265,16 @@ def test(
     sep = np.ones(n, dtype=np.int32)
     is_on_boundary = np.zeros(n, dtype=np.int32)
 
-    @timer
-    def bp_wrapper(*args):
-        err = bp_main(*args)
-        if err < 0:
-            raise(ValueError("Not converged"))
-        return err
-
     err = bp_wrapper(
         lp, lm, l0, Jp, Jm, Jpm, J0, J0p, J0m, 
+        1, 1, 
         x, y,
         hx_xy, hy_xy,
         hx_xpy, hy_xpy,
         hx_xyp, hy_xyp,
         mp_, mm_, xp_, xm_,
         sep, sep_exp,
-        is_on_boundary, n
+        is_on_boundary, opt.lattice_connectivity, n, 1, 1,
     )
 
     print(err)
@@ -216,89 +291,158 @@ def test(
     plt.figure()
     plt.pcolormesh(X, Y, Z, shading='auto', cmap='gray_r', vmax=3)
     plt.show()
+
+def create_mock_data(
+    lp = 1,
+    lm = 1,
+    l0 = 1,
+    Jp = -1,
+    Jm = -1,
+    Jpm = 3,
+    J0 =  0,
+    J0p = 0,
+    J0m = 0,
+    lattice_connectivity = 4
+):
+
+    opt = Optimizer(
+        lattice_connectivity=lattice_connectivity,
+        sel='mock',
+        vol_frac_scaling_x=1,
+        vol_frac_scaling_y=1,
+    )
+
+    xp = np.linspace(0.001, 0.99, 128)
+    yp = np.linspace(0.001, 0.99, 128)
+    X, Y = np.meshgrid(xp, yp)
+    X = X.flatten()
+    Y = Y.flatten()
+    ind = ((X + Y) < 1) & ((X + Y) > 0)
     
+    x = np.ascontiguousarray(X[ind])
+    y = np.ascontiguousarray(Y[ind])
+
+    n = x.size
+
+    hx_xy = np.zeros(n)
+    hy_xy = np.zeros(n)
+    hx_xpy = np.zeros(n)
+    hy_xpy = np.zeros(n)
+    hx_xyp = np.zeros(n)
+    hy_xyp = np.zeros(n)
+    mp_ = np.zeros(n)
+    mm_ = np.zeros(n)
+    xp_ = np.zeros(n)
+    xm_ = np.zeros(n)
+    sep_exp = np.ones(n, dtype=np.int32)
+    sep = np.ones(n, dtype=np.int32)
+    is_on_boundary = np.zeros(n, dtype=np.int32)
+
+    err = bp_wrapper(
+        lp, lm, l0, Jp, Jm, Jpm, J0, J0p, J0m, 
+        1, 1, 
+        x, y,
+        hx_xy, hy_xy,
+        hx_xpy, hy_xpy,
+        hx_xyp, hy_xyp,
+        mp_, mm_, xp_, xm_,
+        sep, sep_exp,
+        is_on_boundary, opt.lattice_connectivity, n, 1, 1,
+    )
+
+    data = np.concatenate([x[:,np.newaxis], y[:,np.newaxis], sep[:,np.newaxis]], axis=1)
+    np.savetxt('mock_data.txt', data)
+
+    Z = -np.ones_like(X)
+    Z[ind] = sep
+    X, Y = np.meshgrid(xp, yp)
+    Z = Z.reshape(X.shape)    
+
+    xx = []
+    yy = []
+    zz = []
+    for i in np.arange(1, 128-1):
+        for j in np.arange(1, 128-1):
+            neighborhood = np.asarray([Z[i-1,j], Z[i+1,j], Z[i,j-1], Z[i,j+1]])
+            notZ = abs(Z[i,j] - 1)
+            if np.any(notZ == neighborhood):
+                xx.append(X[i,j])
+                yy.append(Y[i,j])
+                zz.append(Z[i,j])
+
+    plt.figure()
+    plt.pcolormesh(X, Y, Z, shading='auto', cmap='gray_r', vmax=3)
+
+    xx = np.asarray(xx)
+    yy = np.asarray(yy)
+    zz = np.asarray(zz)
+
+    plt.figure()
+    plt.plot(xx[zz == 0], yy[zz == 0], 's', color='b', alpha=0.3)
+    plt.plot(xx[zz == 1], yy[zz == 1], 's', color='r', alpha=0.3)
+    plt.show()
 
 if __name__ == '__main__':
-    if not args.plot:
-        main()
+    if args.test:
+        test('adp')
+
+    elif args.create_mock_data:
+        create_mock_data()
+
+    elif args.mock:        
+        main_mock()
+
     else:
 
-        if args.molecule == "atp":
-            pass
-            par = []
-            # test(
-            #     'atp',
-            #     0.60644092,   2.02152218,   1.        , -28.04578561,
-            #    -25.45601227,  20.81318829,   0.5831698 ,   8.37261113,
-            #      9.36110971
-            # )
-            # test(
-            #     'atp',
-            #     7.59660748e-02,  8.68736963e+00,  1.00000000e+00, -1.06064513e+01,
-            #    -1.74019484e+01,  4.30497865e+00,  1.94293835e+01,  2.75468133e+01,
-            #     9.11843816e-03
-            # )
-            # test(
-            #     'atp',
-            #     4.29133622,   4.13506414,   1.        ,
-            #     -2.75974958, -27.76296863,  10.00089607, 
-            #     8.41102626,   9.59353239, 4.92079208)
-            # par = [
-            #        0.89242431,   6.17641545, 1.        ,                   
-            #     -9.34106654,  -25.135654  , 6.85812174, 
-            #      29.20649741,   22.89043865, 9.27507924,
-            # ]
-                        # 4.13506414, 4.29133622,    1.        ,
-            # -27.76296863, -2.75974958, 10.00089607, 
-            # 8.41102626,   4.92079208, 9.59353239,
-            # par = [5.967268501277159, 5.482414091116128, 1.0, 
-            # -9.239379014100262, -27.496862291205026, 23.84342682257863, 
-            # 28.662645932889866, 24.98230512512459, 23.631349971142807]
-            par.append([
-                3.2216783435433016, 8.532383036376764, 1.0,
-                -30.80075498859751, -39.65689126829896, 70.06872770454012,
-                36.35896896592931, 52.35004431313808, 50.18379684282572
-            ])
-            par.append([
-                5.0872767041095965, 4.534972342088027, 1.0,
-                -58.14889504948612, -62.13270322269149, 44.494819747364566, 
-                94.60512249360126, 67.22348587054094, 67.98335677618871
-            ])
+        if not args.plot:
+            main()
+        else:
+            params = {}
+            if args.molecule == "mock":
+                pass
+                par = []
+                par.append([
+                    1, 1, 1, -1, -1, 3, 0, 0, 0, 1, 1,
+                    # 1,   1,   1, -2.12919, -8.21704,  10, 4.83072, 5.33092, 5.67371,   1,   1
+                ])
+                params['mock'] = par
 
-        elif args.molecule == "adp":
-            pass
-            par = []
-            # par.append([
-            #     8.3215676 , 2.2923081,    1.        ,
-            #     -1.2394423 , -2.76247692, 21.67804333,
-            #     21.8467445 ,  12.73135856, 29.52351169,
-            # ])
-            par.append([1.84442664, 8.66104699,      1.        , 
-                 -0.79464195, -11.58323324, 14.29676082,
-                 12.54266087,   4.8571578, 20.68036733])
-            par.append([1.84442664,   8.66104699,  1.        , 
-                -0.79464195,  -11.58323324,  14.29676082,
-                 12.54266087,  4.8571578, 20.68036733])
 
-            par.append([0.14989495060633384, 7.7302113553128695, 1.0,
-                 -0.7971531363293334, -13.110360504322951, 25.879187286995972,
-                 3.694158184764616, 0.4519228151825043, 27.67757153055087])
-            par.append([
-                9.335627748627699, 3.551630759332944, 1.0,
-                -12.935181652571138, -45.79489128218752, 53.235138840912846,
-                38.73224218011392, 11.854822890996495, 79.31653941267307
-            ])
-        elif args.molecule == "amp":
-            pass
-            par = []
-            par.append([
-                3.5128614740444695, 4.095576494109553, 1.0, 
-                -4.56390637784709, -23.552059818743686, 0.5736994268111175, 
-                66.90260598124948, 30.819250045900937, 35.32940755299012
-            ])
-            par.append([8.0963607832593, 3.7067654351839607, 1.0,
-             -19.74887434078677, -26.859897609912796, 1.4017760596582391, 
-             84.30873993820705, 32.002524240501344, 52.35361181743063])
+            if args.molecule == "atp" or args.molecule == "all":
+                pass
+                par = []
+                par.append([
+                    # 0.373026, 1.25592,   1, -4.39179, -0.541745, 7.92896, -0.278993, 0.72404, -0.322766, 1.02934, 1
+                    # 6.03091, 7.12607,   1, -1.46297, -9.83835, 2.87021, -0.984717, -0.843357, 0.643944, 99.9025, 4.67344
+                    # 7.50997, 5.29602,   1, -0.887812, -7.12186, 2.08772, 0.712454, -0.0350979, 0.90165, 97.4518, 5.20762
+                ])
+                params['atp'] = par
 
-        for p in par:
-            test(args.molecule, *p)
+            if args.molecule == "adp" or args.molecule == "all":
+                pass
+                par = []
+                par.append([
+                    # 3.05483, 6.91701,   1, -6.2345, -5.90474, 3.71985, -0.914625, -0.0814894, -0.397358, 34.6382, 1
+                    # 7.59671, 4.39042,   1, -9.92094, -3.51605, 1.20798, 0.03642, -0.527211, -0.655306, 90.9876, 1
+                    # 2.34725, 4.22956,   1, -0.0919252, -6.98841, 0.665866, 0.216858, -0.959242, 0.53815, 83.7615, 1
+                    # 3.71504, 3.97799,   1, -4.27913, -3.12154, 4.88684, -0.143496, 0.20706, 0.640943, 64.4663, 0.106799
+                    # 1.93002, 4.17366,   1, -3.36183, -8.36226, 3.39099, 0.787194, 0.516193, -0.0187999, 86.1679, 0.10456
+                    4.34972, 6.10435,   1, -6.55052, -8.62514, 4.75724, -0.820719, -0.0102101, 0.27347, 84.3039, 0.107124
+                ])
+                
+                params['adp'] = par
+
+            if args.molecule == "amp" or args.molecule == "all":
+                pass
+                par = []
+                par.append([
+                    # 3.84697, 0.437053,   1, -7.13027, -3.27763, 8.36572, -0.704957, 0.784959, -0.455163, 1.05849, 1
+                    # 1.91885, 9.47034,   1, -8.98172, -4.82571, 9.8982, -0.249436, -0.335888, 0.303387, 99.4064, 0.108451
+                    7.11278, 6.24691,   1, -3.50006, -6.11359, 9.56895, 0.84808, 0.165324, 0.561215, 99.4149, 0.108503
+                ])
+                params['amp'] = par
+
+            for mol in params:
+                par = params[mol]
+                for p in par:
+                    test(mol, *p)
